43688bbebfc3d2d07f8350a9b43136344a2c6ade
Jiashen G. Lu <20jgl3@lohani.cs.williams.edu>
2020-03-07 20:06:33 -0500

    database initialized

--- SQLiteJDBC.java
+++ SQLiteJDBC.java
@@ -3,6 +3,7 @@ import java.sql.*;
 public class SQLiteJDBC {
     public static void main( String args[] ) {
 	Connection c = null;
+	Statement stmt = null;
 
 	try {
 	    Class.forName("org.sqlite.JDBC");
@@ -12,5 +13,39 @@ public class SQLiteJDBC {
 	    System.exit(0);
 	}
 	System.out.println("Opened database successfully");
+
+	try {
+	    //Class.forName("org.sqlite.JDBC");
+	    //c = DriverManager.getConnection("jdbc:sqlite:test.db");
+	    c.setAutoCommit(false);
+	    System.out.println("Opened database successfully");
+
+	    stmt = c.createStatement();
+	    String sql = "INSERT INTO BOOKS (ID,TITLE,TOPIC,STOCK,PRICE) " +
+			 "VALUES (53477, 'Achieve Less Bugs and More Hugs', 'Distributed Systems', 1,10 );";
+	    stmt.executeUpdate(sql);
+
+	    sql = "INSERT INTO BOOKS (ID,TITLE,TOPIC,STOCK,PRICE) " +
+			 "VALUES (53573, 'Distributed Systems for Dummies', 'Distributed Systems', 1, 10 );";
+	    stmt.executeUpdate(sql);
+
+	    sql = "INSERT INTO BOOKS (ID,TITLE,TOPIC,STOCK,PRICE) " +
+			 "VALUES (12365, 'Surviving College', 'College Life', 1, 10 );";
+	    stmt.executeUpdate(sql);
+	    
+	    sql = "INSERT INTO BOOKS (ID,TITLE,TOPIC,STOCK,PRICE) " +
+			 "VALUES (12498, 'Cooking for the Impatient Undergraduate', 'College Life', 1, 10 );";
+	    stmt.executeUpdate(sql);
+	    
+	    stmt.close();
+	    c.commit();
+	    c.close();
+	} catch ( Exception e ) {
+	    System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+	    System.exit(0);
+	}
+
+
+  
     }
 }
